344792bcb7436c51010dea8f0f4f423aebed6c2b
Joao Paulo Alencar Rodrigues <joao.paulo@inovan.do>
2024-04-24 10:51:59 -0300

    Adds login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Box,
+  FormControl,
+  Button,
+  FormLabel,
+  Input,
+  Text
+} from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box as="main" h="100vh" w="100vw" position="relative">
+      <Box w="40%" h="100%" bgColor="brand.500" />
+      <Box
+        boxShadow="0px 0px 100px #00000070"
+        w="80%"
+        h="80%"
+        position="absolute"
+        m="auto"
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        display="flex"
+      >
+        <Button
+          variant="link"
+          position="absolute"
+          right={10}
+          top={4}
+          fontWeight="medium"
+          color="gray.900"
+        >
+          Need help?
+        </Button>
+        <Box
+          h="100%"
+          w="40%"
+          display="flex"
+          flexDir="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text fontSize="24px" fontWeight="bold" fontStyle="italic">CleanMyCar</Text>
+          <Text mt={4} w="190px" textAlign="center" fontWeight="medium">India's first waterless car cleaning company.</Text>
+        </Box>
+        <Box
+          h="100%"
+          w="60%"
+          display="flex"
+          flexDir="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text fontSize="20px" fontWeight="bold" fontStyle="italic" mb={20}>Log in</Text>
+
+          <FormControl w="50%">
+            <FormLabel>Email</FormLabel>
+            <Input placeholder="john@john.com" />
+          </FormControl>
+          <FormControl w="50%" mt={6}>
+            <FormLabel>Password</FormLabel>
+            <Input placeholder="Enter your password" />
+            <Button
+              variant="link"
+              color="gray.800"
+              fontSize="14px"
+              fontWeight="medium"
+              mt={2}
+              w="100%"
+              justifyContent="flex-end"
+            >
+              forgot password?
+            </Button>
+          </FormControl>
+
+          <Button mt={14} w="50%" colorScheme="brand" color="black" size="lg" fontSize="md" fontWeight="medium">Login</Button>
+        </Box>
+      </Box>
+    </Box >
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import './index.css'
+
+const colors = {
+  brand: {
+    50: "#E8FDF8",
+    100: "#BDF9EA",
+    200: "#93F6DD",
+    300: "#69F2CF",
+    400: "#3FEEC2",
+    500: "#14EBB4",
+    600: "#10BC90",
+    700: "#0C8D6C",
+    800: "#085E48",
+    900: "#042F24"
+  },
+}
+
+const theme = extendTheme({
+  colors,
+  fonts: {
+    body: "Inter, system-ui, sans-serif",
+    heading: "Inter, serif",
+    mono: "Menlo, monospace",
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
